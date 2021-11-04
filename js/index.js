14764e08e39a1b41e1764ccbb7132f15e35f0cd5
ktscates <cates2506@gmail.com>
2021-11-04 10:38:51 +0200

    add playsound logic

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
-window.addEventListener('keydown', function(e){
-    console.log(e)
-})
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+
+    //Stop the function from running
+    if(!audio) return
+
+    //rewind to start
+    audio.currentTime = 0 
+
+    audio.play()
+    key.classList.add("playing")
+}
+
+function removeTransition(e) {
+    // Skip if its not transform
+    if(e.propertyName !== 'transform') return 
+
+    this.classList.remove('playing')
+}
+
+const keys = document.querySelectorAll(".key")
+keys.forEach(key => key.addEventListener("transitionend", removeTransition))
+
+window.addEventListener('keydown', playSound)
